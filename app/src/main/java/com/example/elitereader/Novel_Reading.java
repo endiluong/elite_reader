a39f3fddf0db3b1b419bebf577aca1ae43b396a4
Duy Luong <duy.luong@logixtek.com>
2019-07-28 16:00:14 +0700

    basic view

---
+++ app/src/main/java/com/example/elitereader/Novel_Reading.java
@@ -0,0 +1,13 @@
+package com.example.elitereader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Novel_Reading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novel__reading);
+    }
+}
